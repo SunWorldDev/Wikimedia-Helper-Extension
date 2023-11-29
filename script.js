1c6bfb75e2e007a05e236ab51b49f773345a6d35
Blumenkohl6 <sun-world@eclipso.email>
2023-11-29 22:19:39 +0100

    buggy save system

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+let nextSaveSlot = 0;
+
 document.addEventListener("DOMContentLoaded", function() {
+
+    loadData();
 
     document.getElementById('close').addEventListener("click", (e) => {
         closePopup();
@@ -7,6 +11,11 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('add').addEventListener("click", (e) => {
         openPopup();
     });
+
+    document.getElementById("addBtn").addEventListener("click", (e) => {
+        addFrame();
+    });
+
 });
 
 function openPopup() {
@@ -15,4 +24,40 @@ function openPopup() {
 
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
+    loadData();
+}
+
+function addFrame() {
+
+    saveData()
+
+    nextSaveSlot++;
+
+    closePopup();
+}
+
+async function saveData() {
+    let title = document.getElementById('title').value;
+    let code = document.getElementById('code').value;
+
+    let data = await getData();
+
+    Object.defineProperty(data, title, {
+        value: code
+    });
+
+    chrome.storage.sync.set({ data: data });
+}
+
+function getData() {
+    chrome.storage.sync.get(['data'], function(result) {
+        if (result.data) {
+            return result.data;
+        }
+    });
+    return {};
+}
+
+function loadData() {
+    console.log(getData());
 }
