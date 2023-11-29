764f4f4de4d9180739b2e331cbbaed19b273c299
Blumenkohl6 <sun-world@eclipso.email>
2023-11-29 16:43:58 +0100

    finished GUI

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-chrome.storage.sync.set({ key: value }).then(() => {
-  alert("Value is set");
+chrome.storage.sync.get('overlayColor', function(result) {
+    var get = result.overlayColor;
 });
-
-chrome.storage.sync.get(["key"]).then((result) => {
-  alert("Value currently is " + result.key);
-});
-  
